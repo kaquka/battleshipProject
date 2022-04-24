38c47b6ce317da1b4d33ca9761ff920e9b625919
Kaquka <avalos_luisangel@hotmail.com>
2022-04-24 00:03:45 -0500

    BASE DE DATOS LISTA, en database pueden agregar metodos para que manipulen mas facilmente los datos en la db

---
+++ src/Database/dbCreator.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Database;
+
+import java.sql.Connection;
+import java.sql.*;
+
+/**
+ *
+ * @author avalo
+ */
+public class dbCreator {
+    private static final String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/";
+    private static final String dbName = "sql10487845";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+    private static final String userName = "sql10487845";
+    private static final String password = "Hk79fsHWJJ";
+    
+
+    public void consultaTemplate() {
+        Connection conn = null;
+        
+        try {
+            Class.forName(driver).getDeclaredConstructor().newInstance();
+            conn = DriverManager.getConnection(url+dbName, userName, password);
+
+            if (!conn.isClosed()) {
+                Statement stmt=conn.createStatement();
+                stmt.execute("");
+		//aca se hace la consulta
+            }
+	} catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+	} finally {
+            try {
+		if (conn != null) {
+                    conn.close();
+		}
+            } catch (SQLException e) {
+                System.err.println("Exception: " + e.getMessage());
+            }
+	}
+    }
+
+}
